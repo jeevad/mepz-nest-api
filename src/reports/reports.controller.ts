31a50d9c22d9fce198bb6284d2b49e0071ec9958
Jeeva <jeevanantham.d@janaagraha.org>
2023-07-24 12:17:06 +0530

    Removed unwanted code

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -17,42 +17,6 @@ export class ReportsController {
     res.end(buffer);
   }
 
-  //Get Departments by projectId
-  @Get('getDepartments/:projectId')
-  @ApiOperation({ summary: 'get departments by id' })
-  async getDepartments(
-    @Param('projectId') projectId: string,
-    @Query() { skip, limit, startId }: PaginationParams,
-    @Res() res,
-  ) {
-    const searchQuery = '';
-    const results: any = await this.reportsService.getDepartmentList(
-      projectId,
-      skip,
-      limit,
-      startId,
-      searchQuery,
-    );
-    res.set(this.reportsService.getPdfHeader('department-list', results));
-    res.end(results);
-  }
-
-  //Get Rooms by projectId
-  @Get('getAllRooms/:projectId')
-  @ApiOperation({ summary: 'get rooms by project id' })
-  async getAllRooms(
-    // @Query() paginationParams: PaginationParams,
-    @Param('projectId') projectId: string,
-    @Res() res,
-  ) {
-    const results: any = await this.reportsService.getRoomList(
-      // paginationParams,
-      projectId,
-    );
-    res.set(this.reportsService.getPdfHeader('room-list', results));
-    res.end(results);
-  }
-
   //Get common reports
   @Get('getEquipmentReports')
   @ApiOperation({ summary: 'get rooms by project id' })
@@ -86,36 +50,6 @@ export class ReportsController {
 
     // return results;
     res.set(this.reportsService.getPdfHeader('equipment-list', results));
-    res.end(results);
-  }
-
-  @Get('getAllEquipmentsByDept/:projectId')
-  @ApiOperation({ summary: 'get rooms by project id' })
-  async getAllEquipmentsByDept(
-    @Query() paginationParams: PaginationParams,
-    @Param('projectId') projectId: string,
-    @Res() res,
-  ) {
-    const results: any = await this.reportsService.getAllEquipmentsByDept(
-      projectId,
-    );
-
-    // return results;
-    // res.set(this.reportsService.getPdfHeader('equipment-list', results));
-    res.end(results);
-  }
-
-  @Get('getAllEquipmentsByDeptAndRoom/:projectId')
-  @ApiOperation({ summary: 'get rooms by project id' })
-  async getAllEquipmentsByDeptAndRoom(
-    @Param('projectId') projectId: string,
-    @Res() res,
-  ) {
-    const results: any =
-      await this.reportsService.getAllEquipmentsByDeptAndRoom(projectId);
-
-    // return results;
-    // res.set(this.reportsService.getPdfHeader('equipment-list', results));
     res.end(results);
   }
 }
