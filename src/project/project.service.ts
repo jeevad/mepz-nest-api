45c693be6096ad3a44ea03e5f6dd561dd5d5c92f
VenkateshSaitech20 <venkateshbabu.saitech@gmail.com>
2023-08-19 10:37:45 +0530

    Report pdf -  Total equipment, sub total, price and group field dynamic and pagewise pdf

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1332,7 +1332,7 @@ export class ProjectService {
           room_code: { $first: '$departments.rooms.code' },
           room_name: { $first: '$departments.rooms.name' },
           project_name: { $first: '$name' },
-          
+          group : {$first: '$departments.rooms.equipments.group'}
         },
       },
     ];
