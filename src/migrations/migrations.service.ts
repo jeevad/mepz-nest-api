5dc1e169ea6ef7eb461d9a3f814fd26a054ac825
tutorialshore <benfitsvideos@gmail.com>
2023-08-24 18:03:21 +0530

    master migrate

--- src/migrations/migrations.service.ts
+++ src/migrations/migrations.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DepartmentService } from 'src/master-file/department/department.service';
 import { GroupService } from 'src/master-file/group/group.service';
+import { RoomsService } from 'src/master-file/rooms/rooms.service';
+import { CurrencyService } from 'src/master-file/currency/currency.service';
+import { CompanyService } from 'src/master-file/company/company.service';
+import { UtilityService } from 'src/master-file/utility/utility.service';
+import { EquipmentService } from 'src/master-file/equipment/equipment.service';
+import { PackageService } from 'src/master-file/package/package.service';
+import { ClassificationService } from 'src/master-file/classification/classification.service';
+import { EquipmentBrandService } from 'src/master-file/equipment-brand/equipment-brand.service';
 import { DataSource } from 'typeorm';
 
 @Injectable()
@@ -10,6 +18,15 @@ export class MigrationsService {
     @InjectDataSource('mysql') private connection: DataSource,
     private readonly groupService: GroupService,
     private readonly departmentService: DepartmentService,
+    private readonly roomsService: RoomsService,
+    private readonly currencyService: CurrencyService,
+    private readonly companyService: CompanyService,
+    private readonly utilityService: UtilityService,
+    private readonly equipmentService: EquipmentService,
+    private readonly packageService: PackageService,
+    private readonly classificationService: ClassificationService,
+    private readonly equipmentBrandService: EquipmentBrandService,
+    
   ) {}
 
   async migrateGroup() {
@@ -39,6 +56,49 @@ export class MigrationsService {
           'dep_code as code, dep_desc as name, inactive as active, date_created as createdAt',
         service: 'departmentService',
       },
+   /*   {
+        name: 'tb_room',
+        fields:
+          'rm_code as code, rm_desc as name, inactive as active, date_created as createdAt',
+        service: 'roomsService',
+      },*/
+      {
+        name: 'tb_prj_currency',
+        fields:
+          'h_code as code, cur_code as name, cur_code as symbol, date_created as created',
+        service: 'currencyService',
+      },
+      {
+        name: 'tb_hs_company',
+        fields:
+          'com_code as code, com_name as name, cur_code as inactive, addr1 as address1, addr2 as address2, city, state, postal, country, pic_path as logo1, pic_path as show1, pic_path2 as logo2, pic_path2 as show2, pic_path3 as logo3, pic_path3 as show3, contact, tel_no as phone, mobile_no as mobile, fax_no as fax, emailadd as email, date_created as created',
+        service: 'companyService',
+      },
+      {
+        name: 'tb_utility',
+        fields:
+          'u_code as code, u_desc as name,  date_created as created',
+        service: 'utilityService',
+      },
+      {
+        name: 'tb_eq_package',
+        fields:
+          'pk_code as code, pk_desc as name, inactive as active,  date_created as created',
+        service: 'packageService',
+      },
+      {
+        name: 'tb_hosp_class',
+        fields:
+          'hcl_code as code, hcl_desc as name, inactive as active, date_created as created, date_created as createdAt',
+        service: 'classificationService',
+      },
+      {
+        name: 'tb_eq_gen_desc',
+        fields:
+        'gd_code as code, gd_desc as name, cost, markup_per as markUp, heat_dissipation as heatDissipation, ict_port as ictPort, bss_port as bssPort, date_created as createdAt, remarks, labels , utility, package , package_remarks as packageRemarks, package as equipmentPackage, labels as equipmentLabel, type_of_power as equipmentPower, file1 as fileOne',
+        service: 'equipmentService',
+      },
+  
     ];
     tables.forEach(async (table) => {
       await this.runMasterCreateQuery(table);
